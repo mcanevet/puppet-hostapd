5809950509a80c579eef4fc3abee09432b0fb422
Mickaẽl Canévet <mickael.canevet@camptocamp.com>
2013-11-25 20:07:53 +0100

    Add more unit tests

--- spec/classes/hostapd_spec.rb
+++ spec/classes/hostapd_spec.rb
@@ -10,4 +10,47 @@ describe 'hostapd' do
     end
   end
 
+  context 'when ssid passed' do
+    let(:params) { {
+      :ssid => 'foo',
+    } }
+
+    it { should create_class('hostapd').with({
+      :ssid           => 'foo',
+      :interface      => nil,
+      :bridge         => nil,
+      :driver         => nil,
+      :hw_mode        => nil,
+      :channel        => nil,
+      :wmm_enabled    => nil,
+      :ieee80211n     => nil,
+      :ht_capab       => nil,
+      :auth_algs      => nil,
+      :wpa            => nil,
+      :wpa_passphrase => nil,
+      :wpa_key_mgmt   => nil,
+      :wpa_pairwise   => nil,
+      :rsn_pairwise   => nil,
+      :version        => :present,
+      :enable         => true,
+      :start          => true,
+      :bssids         => {},
+    })}
+    it { should contain_class('hostapd::install') }
+    it { should contain_class('hostapd::config') }
+    it { should contain_class('hostapd::service') }
+
+  end
+
+  context 'when bssids is not a hash' do
+    let(:params) { {
+      :ssid   => 'foo',
+      :bssids => 'bar',
+    } }
+
+    it 'should fail' do
+      expect { should create_class('hostapd') }.to raise_error(Puppet::Error,/ is not a Hash/)
+    end
+  end
+
 end
